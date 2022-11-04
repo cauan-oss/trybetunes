54a1142050d22eb8a779728e45827371fd045156
Cauan <cauanbarros14@gmail.com>
2022-11-04 11:46:33 -0300

    requisito 4 concluido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends Component {
@@ -27,6 +28,9 @@ class Header extends Component {
       <header data-testid="header-component">
         {isLoading ? <p>Carregando...</p>
           : <p data-testid="header-user-name">{ nome }</p> }
+        <Link data-testid="link-to-search" to="/search">Search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
       </header>
     );
   }
