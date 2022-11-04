e3486afaa202f9636f9b65193c5f00d07bb9b5b3
Cauan <cauanbarros14@gmail.com>
2022-11-03 22:15:30 -0300

    req 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Album from './pages/Album';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route exact path="/search" component={ Search } />
+            <Route exact path="/album/:id" component={ Album } />
+            <Route exact path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+
+    );
   }
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import { link } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nome: '',
+      isValidButton: true,
+      isLoading: false,
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    }, () => {
+      const { nome } = this.state;
+      const numberTre = 3;
+      const emptyValidate = nome.length >= numberTre;
+      this.setState({
+        isValidButton: !emptyValidate,
+      });
+    });
+  };
+
+  redirectNome = async () => { 
+    const { history } = this.props;
+    const { nome, isLoading } = this.state;
+    this.setState({ isLoading: true });
+    await createUser({ name: nome });
+    this.setState({ isLoading: false});
+    history.push('/search');
+  };
+
+  render() {
+    const { nome, isValidButton, isLoading} = this.state;
+    return (
+      <div data-testid="page-login">
+        { isLoading ? <p>Carregando...</p>
+          : (<label htmlFor="user">
+            <span>nome</span>
+            <input
+              value={ nome }
+              onChange={ this.onInputChange }
+              name="nome"
+              id="user"
+              data-testid="login-name-input"
+              type="text"
+            />
+            <button
+              data-testid="login-submit-button"
+              disabled={ isValidButton }
+              onClick={ this.redirectNome }
+              type="button"
+            >
+              Entrar
+
+            </button>
+          </label>)}
+      </div>
+    );
+  }
+}
+
+export default Login;
