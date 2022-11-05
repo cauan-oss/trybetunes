1f40524eac14d5792905d5d07a815bd45d9b238e
Cauan <cauanbarros14@gmail.com>
2022-11-05 11:32:19 -0300

    requisito 6 concluido

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,7 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
@@ -7,6 +9,9 @@ class Search extends Component {
     this.state = {
       artista: '',
       isValidButton: true,
+      returnSearch: [],
+      returnText: '',
+      isLoading: false,
     };
   }
 
@@ -23,8 +28,20 @@ class Search extends Component {
     });
   };
 
+  searchArtist = async () => {
+    this.setState({ isLoading: true });
+    const { artista } = this.state;
+    const test = await searchAlbumsAPI(artista);
+    this.setState({
+      returnSearch: test,
+      returnText: artista,
+      artista: '',
+      isLoading: false,
+    });
+  };
+
   render() {
-    const { artista, isValidButton } = this.state;
+    const { artista, isValidButton, returnSearch, returnText, isLoading } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -42,13 +59,37 @@ class Search extends Component {
           </label>
           <button
             disabled={ isValidButton }
-            onClick={ this.onInputChange }
+            onClick={ this.searchArtist }
             type="button"
             data-testid="search-artist-button"
           >
             Pesquisar
 
           </button>
+          {isLoading
+              || (
+                <span id="resultados">
+                  { returnSearch.length > 0
+                    ? `Resultado de álbuns de: ${returnText}`
+                    : 'Nenhum álbum foi encontrado' }
+                </span>) }
+          { isLoading ? <p>Carregando...</p>
+            : returnSearch.length > 0 && returnSearch.map((art) => (
+              <div id="artista" key={ art.artistId }>
+                <Link
+                  to={ `/album/${art.collectionId}` }
+                  data-testid={ `link-to-album-${art.collectionId}` }
+                >
+                  <p id="titulo">{ art.artistName }</p>
+                  <p>{ art.collectionId }</p>
+                  <p>{ art.collectionName }</p>
+                  <p>{ art.collectionPrice }</p>
+                  <p>{ art.artworkUrl100 }</p>
+                  <p>{ art.releaseDate }</p>
+                  <p>{ art.trackCount }</p>
+                </Link>
+              </div>
+            ))}
         </div>
       </div>
     );
